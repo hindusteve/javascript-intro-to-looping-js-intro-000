37845397ccb9004e50ad3eaf2b35168eb0eae244
Abhijit <abhijit.khanna@live.com>
2017-10-02 19:55:54 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop (array) {
+
+  for (var i = 0; i < 25; i++) {
+    console.log("I am {i} strange loops.")
+  }
+}
+
+function whileLoop (number) {
+  while (number >= 0) {
+    console.log(number)
+  }
+
+  console.log("done")
+}
+
+function doWhileLoop (array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop
+
+  } while (array.length > 0 && maybeTrue())
+
+}
