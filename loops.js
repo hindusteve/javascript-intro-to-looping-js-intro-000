722d75180c8c2e25778723edaee551722f31b715
Abhijit <abhijit.khanna@live.com>
2017-10-02 20:18:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,11 @@
 function forLoop(array) {
 
   for (var i = 0; i < 25; i++) {
-    if (i == 1) then {
+    if (i == 1) {
       console.log("I am 1 strange loop.");
-    } 
-    // else {
-    //   console.log(`I am ${i} strange loops.`);
-    // }
+    }  else {
+      console.log(`I am ${i} strange loops.`);
+    }
   }
 }
 
